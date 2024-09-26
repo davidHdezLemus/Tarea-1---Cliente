71696bdd068008cc78c51f63c553af17002db984
david hdez lemus <drhernandezl01@iesalbarregas.es>
2024-09-26 13:11:22 +0200

    Task 1 - update
    Updated HTML file, leaving only the body with an ID.
    Updated CSS file.
    Updated JS file with better structured and clean code.

--- src/js/script.js
+++ src/js/script.js
@@ -1,77 +1,78 @@
 const bodyElement = document.getElementsByTagName("body")[0];
-const timeElement = document.getElementById("time");
+
 const divElement = document.createElement("div");
+const h1Element = document.createElement("h1");
 const inputElement = document.createElement("input");
 
+divElement.classList.add("centerCounter");
+h1Element.setAttribute("id", "time");
 inputElement.setAttribute("type", "date");
+
 bodyElement.appendChild(divElement);
+
+divElement.appendChild(h1Element);
 divElement.appendChild(inputElement);
 
 let currentDate = new Date();
-let predefinedDate = new Date("11/24/2024");
+let programDate = new Date("11/24/2024");
 
-let difference = predefinedDate.getTime() - currentDate.getTime();
+let difference = programDate - currentDate;
 let interval;
 
-function calculateRemainingTime() {
-  let remainingSeconds = Math.floor(difference / 1000) - 7204;
+function counter() {
+  let remainingSeconds = Math.trunc(difference / 1000) - 7204;
 
-  let seconds = Math.floor(remainingSeconds % 60);
-  let minutes = Math.floor((remainingSeconds / 60) % 60);
-  let hours = Math.floor((remainingSeconds / 3600) % 24);
-  let days = Math.floor((remainingSeconds / 86400) % 30);
-  let months = Math.floor(remainingSeconds / 2592000);
-  let years = Math.floor(remainingSeconds / 31536000);
+  let seconds = Math.trunc(remainingSeconds % 60);
+  let minutes = Math.trunc((remainingSeconds / 60) % 60);
+  let hours = Math.trunc((remainingSeconds / 3600) % 24);
+  let days = Math.trunc((remainingSeconds / 86400) % 30);
+  let months = Math.trunc(remainingSeconds / 2592000);
+  let years = Math.trunc(remainingSeconds / 31536000);
 
-  let remainingTimeText = "";
+  let timeText = "";
 
-  if (years > 0) { remainingTimeText += years + " year(s), "; }
-  if (months > 0) { remainingTimeText += months + " month(s), "; }
-  if (days > 0) { remainingTimeText += days + " day(s), "; }
-  if (hours > 0) { remainingTimeText += hours + " hour(s), "; }
-  if (minutes > 0) { remainingTimeText += minutes + " minute(s), "; }
-  if (seconds > 0) { remainingTimeText += seconds + " second(s)"; }
+  if (years > 0) { timeText += years + " year(s), "; }
+  if (months > 0) { timeText += months + " month(s), "; }
+  if (days > 0) { timeText += days + " day(s), "; }
+  if (hours > 0) { timeText += hours + " hour(s), "; }
+  if (minutes > 0) { timeText += minutes + " minute(s), "; }
+  if (seconds > 0) { timeText += seconds + " second(s)"; }
 
   if (months === 1 || months > 1) {
-      timeElement.classList.remove("orangeColor", "redColor");
-      timeElement.classList.add("greenColor");
+      h1Element.classList.remove("orangeColor", "redColor");
+      h1Element.classList.add("greenColor");
   } else if (days >= 7) {
-      timeElement.classList.remove("greenColor", "redColor");
-      timeElement.classList.add("orangeColor");
+      h1Element.classList.remove("greenColor", "redColor");
+      h1Element.classList.add("orangeColor");
   } else if (days < 7) {
-      timeElement.classList.remove("greenColor", "orangeColor");
-      timeElement.classList.add("redColor");
+      h1Element.classList.remove("greenColor", "orangeColor");
+      h1Element.classList.add("redColor");
   }
 
-  timeElement.innerText = remainingTimeText;
+    h1Element.innerText = timeText;
 
   if (difference <= 0) {
       clearInterval(interval);
-      timeElement.classList.remove("greenColor", "orangeColor", "redColor");
-      timeElement.innerText = "🎉🎈 Happy Birthday!!!!! 🎉🎈";
+      h1Element.classList.remove("greenColor", "orangeColor", "redColor");
+      h1Element.innerText = "🎉🎈 Happy Birthday!!!!! 🎉🎈";
   }
 
   difference -= 1000;
 }
 
-calculateRemainingTime();
-interval = setInterval(calculateRemainingTime, 1000);
+interval = setInterval(counter, 1000);
 
-inputElement.valueAsDate = predefinedDate;
+let selectedDate;
+
 inputElement.addEventListener("change", () => {
-    let selectedDate = new Date(inputElement.value);
+    selectedDate = new Date(inputElement.value);
 
-    if (isNaN(selectedDate.getTime())) {
-        alert("The date format is not valid. Please try again.");
-        inputElement.valueAsDate = predefinedDate;
-        selectedDate = predefinedDate;
-    } else if (selectedDate < currentDate) {
-        alert("The selected date cannot be earlier than the current date.");
-        inputElement.valueAsDate = predefinedDate;
-        selectedDate = predefinedDate;
+    if (selectedDate < currentDate) {
+        alert("You cannot select a date earlier than the current date.");
+        selectedDate = programDate;
     }
 
-    difference = selectedDate.getTime() - currentDate.getTime();
+    difference = selectedDate - currentDate;
 });
 
-calculateRemainingTime();
+counter();
